0f20866738e1f05283d9fe18df5f3a9e5abec8cd
Ifeanyi Madu <supermadu7@gmail.com>
2026-01-29 23:50:35 +0000

    add stripe fields to invoices

---
+++ database/migrations/2026_01_29_000246_add_stripe_fields_to_invoices_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->string('stripe_session_id')->nullable()->after('status');
+            $table->string('stripe_payment_intent_id')->nullable()->after('stripe_session_id');
+            $table->json('payment_metadata')->nullable()->after('stripe_payment_intent_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->dropColumn(['stripe_session_id', 'stripe_payment_intent_id', 'payment_metadata']);
+        });
+    }
+};
